cd02f83e90ebfbd5dda709278125bcf3dcb6c5ef
athit-svnp <athit@pascaliaasia.com>
2025-11-29 12:03:29 +0700

    fix bug libary

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,11 +4,8 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
-  file_selector_windows
   firebase_auth
   firebase_core
-  firebase_storage
-  speech_to_text_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
